ba0e517380212bb78e128773613cc05b6da54f84
Meri Kasprak <mkasprak@mchenry.edu>
2025-09-11 10:34:39 -0500

    loops

---
+++ for.py
@@ -0,0 +1,7 @@
+"""
+    numbers divisible by 5 between 1 and 400
+"""
+
+for i in range (1, 400):
+    if i % 5 == 0:
+        print(i)
---
+++ loops.py
@@ -0,0 +1,27 @@
+"""
+    Working with loops
+"""
+
+#  While loops
+bottles = 99
+while bottles > 1:
+    print(f"{bottles} bottles of beer on the wall")
+    print(f"{bottles} bottles of beer")
+    print(f"Take one down, pass it around")
+    # increment the loop
+    bottles = bottles - 1
+    print(f"{bottles} of beer on the wall\n")
+
+print(f"{bottles} bottle of beer on the wall")
+print(f"{bottles} bottle of beer")
+print(f"Take it down, pass it around")
+# increment the loop
+bottles = bottles - 1
+print(f"{bottles} of beer on the wall\n")
+
+print(f"{bottles} bottles of beer on the wall")
+print(f"{bottles} bottles of beer")
+print(f"Take one down, pass it around")
+# increment the loop
+bottles = bottles - 1
+
