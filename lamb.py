1c38168f9f921ab98f3757ee1e8d9c9ca34c9c2b
Meri Kasprak <mkasprak@mchenry.edu>
2025-09-23 11:29:06 -0500

    madlibs

---
+++ lamb.py
@@ -0,0 +1,31 @@
+"""
+    Returning values from functions
+"""
+
+def setup():
+    print("                                  Mad Libs!")
+    print("Please enter the type of word you are asked for, then I will tell you a story.")
+
+
+def get_words():
+    name = input("Please enter a name:  ")
+    size = input("Please enter a size (sm, med, lrg, etc.):  ")
+    animal = input("Please enter a kind of animal:  ")
+    color = input("Please enter a color:  ")
+
+    story(name, size, animal, color)
+
+def story(name_in, size_in, animal_in, color_in):
+    print(f"{name_in} had a {size_in} {animal_in}")
+    print(f"{size_in} {animal_in}")
+    print(f"{size_in} {animal_in}")
+    print(f"Whose fleece was {color_in} as snow")
+
+    print("Original poem by Sarah Josepha Hale")
+
+def main():
+    setup()
+    get_words()
+
+
+main()
