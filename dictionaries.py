bad510532bed01b6a64fb6bf45f4e29890762917
Meri Kasprak <mkasprak@mchenry.edu>
2025-10-02 10:24:26 -0500

    nato

---
+++ dictionaries.py
@@ -0,0 +1,27 @@
+"""
+
+    dictionary demo
+"""
+
+numbers_japanese = {
+    1: "ichi",
+    2: "ni",
+    3: "san",
+    4: "yon",   # (also "shi", but yon is safer/neutral)
+    5: "go",
+    6: "roku",
+    7: "nana",  # (also "shichi", but nana is safer/neutral)
+    8: "hachi",
+    9: "kyuu",  # (also "ku")
+    10: "juu",
+    11: "juu-ichi",
+    12: "juu-ni",
+    13: "juu-san",
+    14: "juu-yon",
+    15: "juu-go",
+    16: "juu-roku",
+    17: "juu-nana",
+    18: "juu-hachi",
+    19: "juu-kyuu",
+    20: "ni-juu"
+}
